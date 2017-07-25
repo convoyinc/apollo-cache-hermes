c5e79ddfd11d509144545c182b55368240ca526b
Ian MacLeod <ian@nevir.net>
2017-07-24 17:52:51 -0700

    Implement a version of clean that doesn't require git

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-git clean -fdx src/
+FILES_TO_REMOVE=($(find src -name "*.d.ts" -or -name "*.js" -or -name "*.js.map"))
+
+if [[ "${#FILES_TO_REMOVE[@]}" != "0" ]]; then
+  echo
+  for file in "${FILES_TO_REMOVE[@]}"; do
+    echo "  ${file}"
+    rm "${file}"
+  done
+  echo
+fi
