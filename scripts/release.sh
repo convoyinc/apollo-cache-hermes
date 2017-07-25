0674db3e7a6f5c156972d191d95243b4be723ab8
Ian MacLeod <ian@nevir.net>
2017-07-24 19:56:00 -0700

    Only increment when necessary

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,24 +3,39 @@ set -e
 
 source ./scripts/include/node.sh
 
-VERSION_PARTS=($(node <<-end_script
-  const fs = require('fs');
-  const version = JSON.parse(fs.readFileSync('package.json')).version;
-  const match = version.match(/^(\d+)\.(\d+)\.(\d+)(?:-(.+))?$/);
-  for (var i = 1; i < match.length; i++) {
-    if (match[i]) {
-      console.log(match[i]);
+parse_version() {
+  version="${1}"
+  node <<-end_script
+    const match = '${version}'.match(/(\d+)\.(\d+)\.(\d+)(?:-(.+))?$/);
+    for (var i = 1; i < match.length; i++) {
+      if (match[i]) {
+        console.log(match[i]);
+      }
     }
-  }
 end_script
-))
+}
 
-X="${VERSION_PARTS[0]}"
-Y="${VERSION_PARTS[1]}"
-Z="${VERSION_PARTS[2]}"
-PRE="${VERSION_PARTS[3]}"
+# Make sure we have all tags available
+git fetch origin --tags
 
-NEW_VERSION="${X}.${Y}.${CIRCLE_BUILD_NUM}"
+VERSION_TEMPLATE=$(node -p "JSON.parse(fs.readFileSync('package.json')).version")
+CLOSEST_VERSION=$(git describe --abbrev=0 --tags)
+TEMPLATE_PARTS=($(parse_version "${VERSION_TEMPLATE}"))
+CLOSEST_PARTS=($(parse_version "${CLOSEST_VERSION}"))
+echo "template: ${TEMPLATE_PARTS[@]}"
+echo "closest: ${CLOSEST_PARTS[@]}"
+
+X="${TEMPLATE_PARTS[0]}"
+Y="${TEMPLATE_PARTS[1]}"
+if [[ "${TEMPLATE_PARTS[0]}" == "${CLOSEST_PARTS[0]}" && "${TEMPLATE_PARTS[1]}" == "${CLOSEST_PARTS[1]}" ]]; then
+  # No change in X.Y - just increment.
+  Z=$((CLOSEST_PARTS[2] + 1))
+else
+  Z="${TEMPLATE_PARTS[2]}"
+fi
+
+NEW_VERSION="${X}.${Y}.${Z}"
+PRE="${TEMPLATE_PARTS[3]}"
 if [[ "${PRE}" != "" ]]; then
   NEW_VERSION="${NEW_VERSION}-${PRE}"
 fi
