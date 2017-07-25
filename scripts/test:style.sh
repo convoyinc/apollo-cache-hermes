44e6baf3a56c7e217ad9909323c12d9f5237de3b
Ian MacLeod <ian@nevir.net>
2017-07-24 20:21:42 -0700

    test:style via eslint

---
+++ scripts/test:style.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+source ./scripts/include/shell.sh
+source ./scripts/include/node.sh
+
+FILES=("${OPTIONS_ARGS[@]}")
+if [[ "${#FILES[@]}" = "0" ]]; then
+  FILES+=($(
+    find . \
+      -not -path "*/node_modules/*" \
+      \( -name "*.ts" -not -name "*.d.ts" \) \
+      -or -name "*.tsx"
+  ))
+fi
+
+OPTIONS=(
+  "${OPTIONS_FLAGS[@]}"
+)
+
+eslint "${OPTIONS[@]}" "${FILES[@]}"
