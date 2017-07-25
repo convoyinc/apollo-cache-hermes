b6bde1ee1418b3f1d053637f6d942e7050247aa8
Ian MacLeod <ian@nevir.net>
2017-07-25 08:31:27 -0700

    yarn test

---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+source ./scripts/include/shell.sh
+source ./scripts/include/node.sh
+
+run compile
+run test:style "${OPTIONS_ARGS[@]}"
+run test:unit "${OPTIONS_ARGS[@]}"
