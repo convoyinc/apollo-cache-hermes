b488b2fe15ad5fbd2560da05ecab5665530bd889
Ian MacLeod <ian@nevir.net>
2017-08-02 08:15:49 -0700

    Document reads

---
+++ src/operations/read.ts
@@ -0,0 +1,114 @@
+import { SelectionSetNode } from 'graphql'; // eslint-disable-line import/no-extraneous-dependencies, import/no-unresolved
+
+import { GraphSnapshot } from '../GraphSnapshot';
+import { NodeId, StaticNodeId } from '../schema';
+
+/**
+ * Get you some datas.
+ */
+export function read(
+  snapshot: GraphSnapshot,
+  selection: SelectionSetNode,
+  rootId?: NodeId,
+): { result: any, complete: boolean } {
+  let result = snapshot.get(rootId || StaticNodeId.QueryRoot);
+
+  const parameterizedEdges = _parameterizedEdgesForSelection(selection);
+  if (parameterizedEdges) {
+    result = _overlayParameterizedValues(result, snapshot, parameterizedEdges, selection);
+  }
+
+  const complete = _isSelectionSatisfied(selection, result);
+
+  return { result, complete };
+}
+
+/**
+ * A recursive map where the keys indicate the path to any edge in a result set
+ * that contain a parameterized edge.
+ */
+export interface ParameterizedEdgeMap {
+  [Key: string]: ParameterizedEdgeMap | true;
+}
+
+/**
+ * Walks a selection set, identifying the path to all parameterized edges.
+ */
+export function _parameterizedEdgesForSelection(selection: SelectionSetNode): ParameterizedEdgeMap | undefined {
+  // Rough algorithm is as follows:
+  //
+  //   * Return memoized value for `selection`, if present.
+  //
+  //   * Visit each node of `selection`, keeping track of the current path:
+  //
+  //     * If a FieldNode with arguments is encountered, deep set the path taken
+  //       to it in the edge map with a value of true.
+  //
+  //   * Return the edge map, if any.
+  //
+  // This has some room for improvement.  We can likely cache per-fragment, or
+  // per-node (particularly with some knowledge of the underlying schema).
+
+  // Random line to get ts/tslint to shut up.
+  return _parameterizedEdgesForSelection(selection);
+}
+
+/**
+ * Walks a parameterized edge map, overlaying values at those paths on top of
+ * existing results.
+ *
+ * Overlaid values are objects with prototypes pointing to the original results,
+ * and new properties pointing to the parameterized values (or objects that
+ * contain them).
+ */
+export function _overlayParameterizedValues<TResult>(
+  result: TResult,
+  snapshot: GraphSnapshot,
+  edges: ParameterizedEdgeMap,
+  selection: SelectionSetNode,
+): TResult {
+  // Rough algorithm is as follows:
+  //
+  //   * Visit each node of `edges`, along side `result`:
+  //
+  //     * Determine the node id (if any), and track it along side the current
+  //       path.
+  //
+  //     * If we've reached a value of `true` in the edge map:
+  //
+  //       * Determine the id of the parameterized edge (closest nodeId +
+  //         parameters).
+  //
+  //       * Fetch the value of that node from the snapshot, and assign it to
+  //         the newly constructed parent (see the next step).
+  //
+  //     * Otherwise, construct a new object whose prototype is the value of the
+  //       results at this path.
+  //
+  // Return the new copy of the results with overlaid edges.
+  //
+
+  // Random line to get ts/tslint to shut up.
+  return _overlayParameterizedValues(result, snapshot, edges, selection);
+}
+
+/**
+ * Determines whether `result` satisfies the properties requested  `selection`.
+ */
+export function _isSelectionSatisfied(selection: SelectionSetNode, result: any): boolean {
+  //  Rough algorithm is as follows:
+  //
+  //   * Visit each node of `selection` and `result`:
+  //
+  //     * For each property expressed by the current node of `selection`:
+  //
+  //       * If !(property in resultNode) return false
+  //
+  //   * return true
+  //
+  //  This has a lot of room for improvement.  We can likely cache per-fragment,
+  //  or per-node.
+
+  // Random line to get ts/tslint to shut up.
+  return _isSelectionSatisfied(selection, result);
+}
