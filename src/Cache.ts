dfb40f4498a6cd806d224afaae5109dfa3261c41
Ian MacLeod <ian@nevir.net>
2017-08-02 20:06:21 -0700

    Add watch() support

--- src/Cache.ts
+++ src/Cache.ts
@@ -3,10 +3,11 @@ import { DocumentNode } from 'graphql'; // eslint-disable-line import/no-extrane
 import { getQueryDefinitionOrDie, getSelectionSetOrDie } from './ast';
 import { CacheSnapshot } from './CacheSnapshot';
 import { Configuration } from './Configuration';
-import { read, SnapshotEditor } from './operations';
-import { ChangeId, NodeId } from './schema';
+import { GraphSnapshot } from './GraphSnapshot';
+import { Query, QueryObserver, read, SnapshotEditor } from './operations';
+import { ChangeId, NodeId, StaticNodeId } from './schema';
 
-export interface ReadOptions {
+export interface ApolloReadOptions {
   query: DocumentNode;
   variables: object;
   optimistic: boolean;
@@ -14,16 +15,15 @@ export interface ReadOptions {
   previousResult?: any;
 }
 
-export interface WriteOptions {
+export interface ApolloWriteOptions {
   dataId: string;
   result: any;
   document: DocumentNode;
   variables?: object;
 }
 
-export interface Transaction {
-  (cache: Cache): void;
-}
+export type Transaction = (cache: Cache) => void;
+export type Unsubscribe = () => void;
 
 /**
  * The Hermes cache.
@@ -39,30 +39,41 @@ export class Cache {
   /** The current version of the cache. */
   private _snapshot: CacheSnapshot;
 
+  /** All active query observers. */
+  private _observers: QueryObserver[];
+
   /**
    * Reads the selection expressed by a query from the cache.
    */
-  read(options: ReadOptions): { result: any, complete: boolean } {
+  read(options: ApolloReadOptions): { result: any, complete: boolean } {
     // TODO: Can we drop non-optimistic reads?
     // https://github.com/apollographql/apollo-client/issues/1971#issuecomment-319402170
     const snapshot = options.optimistic ? this._snapshot.optimistic : this._snapshot.baseline;
-    const query = getQueryDefinitionOrDie(options.query);
+    const query = _queryForReadOptionsOrDie(options);
 
-    return read(this._config, snapshot, query.selectionSet);
+    return read(this._config, query, snapshot);
   }
 
   /**
    *
    */
-  watch(options: ReadOptions, callback: () => void): void {
-    // Random line to get ts/tslint to shut up.
-    return this.watch(options, callback);
+  watch(options: ApolloReadOptions, callback: () => void): Unsubscribe {
+    if (!options.optimistic) {
+      // Apollo Client doesn't ever call with `optimistic: false`, today.
+      throw new Error(`Cache#watch is not supported for non-optimistic queries`);
+    }
+
+    const query = _queryForReadOptionsOrDie(options);
+    const observer = new QueryObserver(this._config, query, this._snapshot.optimistic, callback);
+    this._observers.push(observer);
+
+    return () => this._removeObserver(observer);
   }
 
   /**
    * Writes values for a selection to the cache.
    */
-  write(options: WriteOptions): void {
+  write(options: ApolloWriteOptions): void {
     const selection = getSelectionSetOrDie(options.document);
     const currentSnapshot = this._snapshot;
 
@@ -79,7 +90,7 @@ export class Cache {
       }
     }
 
-    // TODO: Let observers know about editedNodeIds.
+    this._broadcastChanges(optimistic, editedNodeIds);
 
     this._snapshot = { baseline, optimistic, optimisticStateQueue: currentSnapshot.optimisticStateQueue };
   }
@@ -116,4 +127,36 @@ export class Cache {
     return this.removeOptimistic(id);
   }
 
+  // Internal
+
+  /**
+   *
+   */
+  private _removeObserver(observer: QueryObserver): void {
+    const index = this._observers.findIndex(o => o === observer);
+    this._observers.splice(index, 1);
+  }
+
+  /**
+   *
+   */
+  private _broadcastChanges(snapshot: GraphSnapshot, changedNodeIds: Set<NodeId>): void {
+    for (const observer of this._observers) {
+      observer.consumeChanges(snapshot, changedNodeIds);
+    }
+  }
+
+}
+
+/**
+ *
+ */
+export function _queryForReadOptionsOrDie(options: ApolloReadOptions): Query {
+  const queryNode = getQueryDefinitionOrDie(options.query);
+
+  return {
+    rootId: StaticNodeId.QueryRoot,
+    selection: queryNode.selectionSet,
+    variables: options.variables,
+  };
 }
