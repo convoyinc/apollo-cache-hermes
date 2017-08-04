2d92823d47eac50ba2179be573d33b20ab9bf11b
Ian MacLeod <ian@nevir.net>
2017-08-03 19:59:48 -0700

    Wrap write() around SnapshotEditor

--- src/operations/write.ts
+++ src/operations/write.ts
@@ -1,12 +1,20 @@
 import { Configuration } from '../Configuration';
 import { GraphSnapshot } from '../GraphSnapshot';
-import { NodeId, Query } from '../schema';
+import { Query } from '../schema';
+
+import { EditedSnapshot, SnapshotEditor } from './SnapshotEditor';
 
 /**
+ * Merges a payload with an existing graph snapshot, generating a new one.
  *
+ * Performs the minimal set of edits to generate new immutable versions of each
+ * node, while preserving immutability of the parent snapshot.
  */
-export function write(
-  config: Configuration, snapshot: GraphSnapshot, query: Query, payload: any
-): { snapshot: GraphSnapshot, editedNodeIds: Set<NodeId> } {
-  return write(config, snapshot, query, payload);
+export function write(config: Configuration, snapshot: GraphSnapshot, query: Query, payload: any): EditedSnapshot {
+  // We _could_ go purely functional with the editor, but it's honestly pretty
+  // convenient to follow the builder function instead - it'd end up passing
+  // around a context object anyway.
+  const editor = new SnapshotEditor(config, snapshot);
+  editor.mergePayload(query, payload);
+  return editor.commit();
 }
