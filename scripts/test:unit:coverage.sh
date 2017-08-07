22c3bedc63f9ff38d12a3661196bafeab370a60b
Ian MacLeod <ian@nevir.net>
2017-08-07 08:58:59 -0700

    Run the html reporter when testing with coverage locally

--- scripts/test:unit:coverage.sh
+++ scripts/test:unit:coverage.sh
@@ -3,8 +3,17 @@ set -e
 
 source ./scripts/include/node.sh
 
-run test:unit -- --coverage
+OPTIONS=()
+if [[ "${CI}" == "" ]]; then
+  OPTIONS+=(
+    --coverageReporters html
+  )
+fi
 
-if [[ "${CI}" != "" ]]; then
+run test:unit -- --coverage "${OPTIONS[@]}"
+
+if [[ "${CI}" == "" ]]; then
+  open ./output/test:unit/index.html
+else
   codecov --file=./output/test:unit/lcov.info
 fi
