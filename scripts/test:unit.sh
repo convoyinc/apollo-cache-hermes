e993c6d0133788386c1b8d7bafbf608de8719826
Ian MacLeod <ian@nevir.net>
2017-08-07 09:23:18 -0700

    Compile before tests during local dev

--- scripts/test:unit.sh
+++ scripts/test:unit.sh
@@ -40,4 +40,9 @@ done
 export JEST_SUITE_NAME="test:unit"
 export JEST_JUNIT_OUTPUT=./output/test:unit/report.xml
 
+# On remote test passes, we build once up front; and run/release based on that.
+if [[ "${CI}" == "" ]]; then
+  run compile
+fi
+
 node "${NODE_OPTIONS[@]}" ./node_modules/.bin/jest "${OPTIONS[@]}" "${FILES[@]}"
