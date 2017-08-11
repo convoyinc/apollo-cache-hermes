70d401f23a4375f56ad99e86174632ba95fb0ef6
Ian MacLeod <ian@nevir.net>
2017-08-11 16:13:53 -0700

    Tests for QueryInfo

---
+++ test/unit/context/QueryInfo.ts
@@ -0,0 +1,63 @@
+import { ParameterizedEdge, VariableArgument } from '../../../src/util';
+import { DocumentNode, NameNode } from 'graphql'; // eslint-disable-line import/no-extraneous-dependencies, import/no-unresolved
+import gql from 'graphql-tag';
+
+import { QueryInfo } from '../../../src/context/QueryInfo';
+
+describe(`context.QueryInfo`, () => {
+
+  describe(`with a valid query document`, () => {
+
+    let query: DocumentNode, info: QueryInfo;
+    beforeAll(() => {
+      query = gql`
+        fragment completeStuff on Stuff {
+          id
+          name
+        }
+
+        query getThings($ids: [ID]!) {
+          stuff { ...completeStuff }
+          things(ids: $ids) {
+            ...completeThing
+          }
+        }
+
+        fragment completeThing on Thing {
+          id
+          name
+          extra
+        }
+      `;
+
+      info = new QueryInfo(query);
+    });
+
+    it(`hangs onto the document, with no changes`, () => {
+      expect(info.document).to.eq(query);
+    });
+
+    it(`extracts the operation`, () => {
+      expect((info.operation.name as NameNode).value).to.eq('getThings');
+      expect(info.operation.operation).to.eq('query');
+    });
+
+    it(`extracts the operation name`, () => {
+      expect(info.operationName).to.eq('getThings');
+    });
+
+    it(`builds a fragment map`, () => {
+      expect(info.fragmentMap).to.have.all.keys('completeStuff', 'completeThing');
+    });
+
+    it(`builds a parameterized edge map`, () => {
+      expect(info.parameterizedEdgeMap).to.deep.eq({
+        things: new ParameterizedEdge({
+          ids: new VariableArgument('ids'),
+        }),
+      });
+    });
+
+  });
+
+});
