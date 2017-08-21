beb9ff0c01a0311993f5ea05a69621bbad51ffd1
Ian MacLeod <ian@nevir.net>
2017-08-21 15:29:15 -0700

    Don't clean scripts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,7 @@ set -e
 FILES_TO_REMOVE=($(
   find . \
     \( -name "*.d.ts" -or -name "*.js" -or -name "*.js.map" \) \
+    -not -path "./scripts/*" \
     -not -path "./coverage/*" \
     -not -path "./node_modules/*" \
     -not -path "./typings/*"
