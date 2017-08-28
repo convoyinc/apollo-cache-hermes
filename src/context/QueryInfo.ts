ed2bfaa99a4dac59104895fba8cce68c67557372
Ian MacLeod <ian@nevir.net>
2017-08-28 15:39:18 -0700

    Validate queries

--- src/context/QueryInfo.ts
+++ src/context/QueryInfo.ts
@@ -1,10 +1,11 @@
 import { // eslint-disable-line import/no-extraneous-dependencies, import/no-unresolved
   DocumentNode,
   OperationDefinitionNode,
+  OperationTypeNode,
 } from 'graphql';
 
 import { DynamicFieldMap, buildDynamicFieldMap } from '../DynamicField';
-import { FragmentMap, fragmentMapForDocument, getOperationOrDie } from '../util';
+import { FragmentMap, fragmentMapForDocument, getOperationOrDie, variablesInOperation } from '../util';
 
 /**
  * Metadata about a GraphQL document (query/mutation/fragment/etc).
@@ -18,6 +19,8 @@ export class QueryInfo {
   public readonly document: DocumentNode;
   /** The primary operation in the document. */
   public readonly operation: OperationDefinitionNode;
+  /** The type of operation. */
+  public readonly operationType: OperationTypeNode;
   /** The name of the operation. */
   public readonly operationName?: string;
   /** All fragments in the document, indexed by name. */
@@ -33,12 +36,43 @@ export class QueryInfo {
   constructor(document: DocumentNode) {
     this.document = document;
     this.operation = getOperationOrDie(document);
+    this.operationType = this.operation.operation;
     this.operationName = this.operation.name && this.operation.name.value;
     this.fragmentMap = fragmentMapForDocument(document);
 
     const { fieldMap, variables } = buildDynamicFieldMap(this.fragmentMap, this.operation.selectionSet);
     this.dynamicFieldMap = fieldMap;
     this.variables = variables;
+
+    this._assertValid();
+  }
+
+  private _assertValid() {
+    const messages: string[] = [];
+
+    const declaredVariables = variablesInOperation(this.operation);
+    this._assertAllVariablesDeclared(messages, declaredVariables);
+    this._assertAllVariablesUsed(messages, declaredVariables);
+
+    if (!messages.length) return;
+    const mainMessage = `Validation errors in ${this.operationType} ${this.operationName || '<unknown>'}`;
+    throw new Error(`${mainMessage}:${messages.map(m => `\n * ${m}`).join('')}`);
+  }
+
+  private _assertAllVariablesDeclared(messages: string[], declaredVariables: Set<string>) {
+    for (const name of this.variables) {
+      if (!declaredVariables.has(name)) {
+        messages.push(`Variable $${name} is used, but not declared`);
+      }
+    }
+  }
+
+  private _assertAllVariablesUsed(messages: string[], declaredVariables: Set<string>) {
+    for (const name of declaredVariables) {
+      if (!this.variables.has(name)) {
+        messages.push(`Variable $${name} is unused`);
+      }
+    }
   }
 
 }
