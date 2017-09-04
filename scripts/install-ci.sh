9414214b8efd02d1fba9905db0f8b674d08e354e
Ian MacLeod <ian@nevir.net>
2017-09-04 12:24:18 -0700

    Don't read or write a lockfile for now

--- scripts/install-ci.sh
+++ scripts/install-ci.sh
@@ -5,13 +5,5 @@ set -e
 # build process restores them only when package.json has not changed.
 [[ -d ./node_modules ]] && exit 0
 
-# If this is a Greenkeeper build, make sure to update the lockfile before
-# installing dependencies.
-if [[ "${CIRCLE_BRANCH}" =~ ^greenkeeper/ ]]; then
-  sudo yarn global add greenkeeper-lockfile
-  greenkeeper-lockfile-update
-  greenkeeper-lockfile-upload
-fi
-
 # Finally, install.
-yarn install --frozen-lockfile
+yarn install --no-lockfile
