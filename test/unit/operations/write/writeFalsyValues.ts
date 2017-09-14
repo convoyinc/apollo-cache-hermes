b10720c864fa0a10cd4bd13b1af7c6367a99a949
Yui T <yui@convoy.com>
2017-09-14 10:22:36 -0700

    Using correct query to write falsy value

--- test/unit/operations/write/writeFalsyValues.ts
+++ test/unit/operations/write/writeFalsyValues.ts
@@ -1,7 +1,7 @@
 import { CacheContext } from '../../../../src/context';
 import { GraphSnapshot } from '../../../../src/GraphSnapshot';
 import { write } from '../../../../src/operations/write';
-import { NodeId, Query, StaticNodeId } from '../../../../src/schema';
+import { NodeId, StaticNodeId } from '../../../../src/schema';
 import { query, strictConfig } from '../../../helpers';
 
 const { QueryRoot: QueryRootId } = StaticNodeId;
@@ -14,14 +14,13 @@ describe(`operations.write`, () => {
 
   const context = new CacheContext(strictConfig);
   const empty = new GraphSnapshot();
-  const rootValuesQuery = query(`{ foo bar }`);
 
   describe(`write falsy values`, () => {
-    let falsyValuesQuery: Query, snapshot: GraphSnapshot, editedNodeIds: Set<NodeId>;
+    let snapshot: GraphSnapshot, editedNodeIds: Set<NodeId>;
     beforeAll(() => {
-      falsyValuesQuery = query(`{ null, false, zero, string }`);
+      const falsyValuesQuery = query(`{ null, false, zero, string }`);
 
-      const result = write(context, empty, rootValuesQuery, { null: null, false: false, zero: 0, string: '' });
+      const result = write(context, empty, falsyValuesQuery, { null: null, false: false, zero: 0, string: '' });
       snapshot = result.snapshot;
       editedNodeIds = result.editedNodeIds;
     });
