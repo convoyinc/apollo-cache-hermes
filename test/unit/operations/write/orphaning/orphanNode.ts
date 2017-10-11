f511e3c1e42e29f528d8dc527c7b4b00ccd5d8f4
Yui T <yui@convoy.com>
2017-10-11 15:13:40 -0700

    Move file into its own folder

---
+++ test/unit/operations/write/orphaning/orphanNode.ts
@@ -0,0 +1,66 @@
+import { CacheContext } from '../../../../src/context';
+import { GraphSnapshot } from '../../../../src/GraphSnapshot';
+import { write } from '../../../../src/operations/write';
+import { NodeId, StaticNodeId } from '../../../../src/schema';
+import { query, strictConfig } from '../../../helpers';
+
+const { QueryRoot: QueryRootId } = StaticNodeId;
+
+// These are really more like integration tests, given the underlying machinery.
+//
+// It just isn't very fruitful to unit test the individual steps of the write
+// workflow in isolation, given the contextual state that must be passed around.
+describe(`operations.write`, () => {
+
+  const context = new CacheContext(strictConfig);
+  const empty = new GraphSnapshot();
+  const rootValuesQuery = query(`{
+    foo {
+      id
+      name
+    }
+    bar {
+      id
+      name
+    }
+  }`);
+
+  describe(`when orphaning a node`, () => {
+    let baseline: GraphSnapshot, snapshot: GraphSnapshot, editedNodeIds: Set<NodeId>;
+    beforeAll(() => {
+      const baselineResult = write(context, empty, rootValuesQuery, {
+        foo: { id: 1, name: 'Foo' },
+        bar: { id: 2, name: 'Bar' },
+      });
+      baseline = baselineResult.snapshot;
+
+      const result = write(context, baseline, query(`{ bar { id } }`), {
+        bar: null,
+      });
+      snapshot = result.snapshot;
+      editedNodeIds = result.editedNodeIds;
+    });
+
+    it(`replaces the reference with null`, () => {
+      expect(snapshot.getNodeData(QueryRootId)).to.deep.eq({
+        foo: { id: 1, name: 'Foo' },
+        bar: null,
+      });
+    });
+
+    it(`updates outbound references`, () => {
+      const queryRoot = snapshot.getNodeSnapshot(QueryRootId)!;
+      expect(queryRoot.outbound).to.have.deep.members([{ id: '1', path: ['foo'] }]);
+    });
+
+    it(`marks the container and orphaned node as edited`, () => {
+      expect(Array.from(editedNodeIds)).to.have.members([QueryRootId, '2']);
+    });
+
+    it(`contains the correct nodes`, () => {
+      expect(snapshot.allNodeIds()).to.have.members([QueryRootId, '1']);
+    });
+
+  });
+
+});
---
+++ test/unit/operations/write/orphaning/orphanSubgraph.ts
@@ -0,0 +1,152 @@
+import { CacheContext } from '../../../../src/context';
+import { GraphSnapshot } from '../../../../src/GraphSnapshot';
+import { write } from '../../../../src/operations/write';
+import { NodeId, StaticNodeId } from '../../../../src/schema';
+import { query, strictConfig } from '../../../helpers';
+
+const { QueryRoot: QueryRootId } = StaticNodeId;
+
+// These are really more like integration tests, given the underlying machinery.
+//
+// It just isn't very fruitful to unit test the individual steps of the write
+// workflow in isolation, given the contextual state that must be passed around.
+describe(`operations.write`, () => {
+
+  const context = new CacheContext(strictConfig);
+  const empty = new GraphSnapshot();
+  const rootValuesQuery = query(`{
+    foo {
+      id
+      name
+    }
+    bar {
+      id
+      name
+    }
+  }`);
+
+  describe(`when orphaning a node`, () => {
+    let baseline: GraphSnapshot, snapshot: GraphSnapshot, editedNodeIds: Set<NodeId>;
+    beforeAll(() => {
+      const baselineResult = write(context, empty, rootValuesQuery, {
+        foo: { id: 1, name: 'Foo' },
+        bar: { id: 2, name: 'Bar' },
+      });
+      baseline = baselineResult.snapshot;
+
+      const result = write(context, baseline, query(`{ bar { id } }`), {
+        bar: null,
+      });
+      snapshot = result.snapshot;
+      editedNodeIds = result.editedNodeIds;
+    });
+
+    it(`replaces the reference with null`, () => {
+      expect(snapshot.getNodeData(QueryRootId)).to.deep.eq({
+        foo: { id: 1, name: 'Foo' },
+        bar: null,
+      });
+    });
+
+    it(`updates outbound references`, () => {
+      const queryRoot = snapshot.getNodeSnapshot(QueryRootId)!;
+      expect(queryRoot.outbound).to.have.deep.members([{ id: '1', path: ['foo'] }]);
+    });
+
+    it(`marks the container and orphaned node as edited`, () => {
+      expect(Array.from(editedNodeIds)).to.have.members([QueryRootId, '2']);
+    });
+
+    it(`contains the correct nodes`, () => {
+      expect(snapshot.allNodeIds()).to.have.members([QueryRootId, '1']);
+    });
+
+  });
+
+  describe(`when orphaning a subgraph`, () => {
+    let baseline: GraphSnapshot, snapshot: GraphSnapshot, editedNodeIds: Set<NodeId>;
+    beforeAll(() => {
+      const subgraphQuery = query(`{
+        foo {
+          id
+          name
+          two {
+            id
+          }
+        }
+        bar {
+          id
+          one {
+            id
+          }
+          two {
+            id
+          }
+          three {
+            id
+            foo {
+              id
+            }
+          }
+        }
+      }`);
+      const baselineResult = write(context, empty, subgraphQuery, {
+        foo: {
+          id: 1,
+          name: 'Foo',
+          two: { id: 222 },
+        },
+        bar: {
+          id: 2,
+          one: { id: 111 },
+          two: { id: 222 },
+          three: {
+            id: 333,
+            foo: { id: 1 },
+          },
+        },
+      });
+      baseline = baselineResult.snapshot;
+
+      const result = write(context, baseline, subgraphQuery, {
+        foo: {
+          id: 1,
+          name: 'Foo',
+          two: null,
+        },
+        bar: null,
+      });
+      snapshot = result.snapshot;
+      editedNodeIds = result.editedNodeIds;
+    });
+
+    it(`replaces the reference with null`, () => {
+      expect(snapshot.getNodeData(QueryRootId)).to.deep.eq({
+        foo: {
+          id: 1,
+          name: 'Foo',
+          two: null,
+        },
+        bar: null,
+      });
+    });
+
+    it(`preserves nodes that only lost some of their inbound references`, () => {
+      expect(snapshot.getNodeData('1')).to.deep.eq({ id: 1, name: 'Foo', two: null });
+    });
+
+    it(`updates outbound references`, () => {
+      const queryRoot = snapshot.getNodeSnapshot(QueryRootId)!;
+      expect(queryRoot.outbound).to.have.deep.members([{ id: '1', path: ['foo'] }]);
+    });
+
+    it(`marks the container and all orphaned nodes as edited`, () => {
+      expect(Array.from(editedNodeIds)).to.have.members([QueryRootId, '1', '2', '111', '222', '333']);
+    });
+
+    it(`contains the correct nodes`, () => {
+      expect(snapshot.allNodeIds()).to.have.members([QueryRootId, '1']);
+    });
+
+  });
+});
