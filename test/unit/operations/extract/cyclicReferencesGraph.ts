50ed7fc5ade873921571848d723c27356e11f7e2
Yui T <yui@convoy.com>
2017-10-13 17:25:57 -0700

    extract: cyclic and duplicate graph tests

---
+++ test/unit/operations/extract/cyclicReferencesGraph.ts
@@ -0,0 +1,68 @@
+import { extract } from '../../../../src/operations/extract';
+import { Serializeable } from '../../../../src/primitive';
+import { StaticNodeId } from '../../../../src/schema';
+import { createSnapshot } from '../../../helpers';
+
+const { QueryRoot: QueryRootId } = StaticNodeId;
+
+describe.skip(`operations.serialization`, () => {
+  describe(`cyclic GraphSnapshot`, () => {
+
+    let extractResult: Serializeable.GraphSnapshot;
+    beforeAll(() => {
+      const snapshot = createSnapshot(
+        {
+          foo: {
+            id: 1,
+            name: 'Foo',
+            bar: {
+              id: 2,
+              name: 'Bar',
+              fizz: { id: 1 },
+              buzz: { id: 2 },
+            },
+          },
+        },
+        `{
+          foo {
+            id
+            name
+            bar {
+              id
+              name
+              fizz { id }
+              buzz { id }
+            }
+          }
+        }`
+      ).snapshot;
+
+      extractResult = extract(snapshot);
+    });
+
+    it(`extract Json serialization object`, () => {
+      expect(extractResult).to.deep.eq({
+        [QueryRootId]: {
+          inbound: null,
+          outbound: null,
+          data: null,
+        },
+        '1': {
+          data: { id: 1, name: 'Foo' },
+          inbound: [
+            { id: 2, path: ['fizz'] },
+          ],
+          outbound: [],
+        },
+        '2': {
+          data: { id: 2, name: 'Bar' },
+          inbound: [
+            { id: 2, path: ['buzz'] },
+          ],
+          outbound: [],
+        },
+      });
+    });
+
+  });
+});
---
+++ test/unit/operations/extract/duplicateReferencesGraph.ts
@@ -0,0 +1,90 @@
+import { extract } from '../../../../src/operations/extract';
+import { Serializeable } from '../../../../src/primitive';
+import { StaticNodeId } from '../../../../src/schema';
+import { createSnapshot } from '../../../helpers';
+
+const { QueryRoot: QueryRootId } = StaticNodeId;
+
+describe.skip(`operations.serialization`, () => {
+  describe(`duplicate GraphSnapshot`, () => {
+
+    let extractResult: Serializeable.GraphSnapshot;
+    beforeAll(() => {
+      const snapshot = createSnapshot(
+        {
+          foo: [
+            { id: 'a', bar: { id: 1 } },
+            { id: 'a', bar: { id: 1 } },
+            { id: 'b', bar: { id: 1 } },
+            { id: 'a', bar: { id: 1 } },
+            { id: 'b', bar: { id: 1 } },
+          ],
+          baz: {
+            id: 'a', bar: { id: 1 },
+          },
+        },
+        `{
+          foo {
+            id
+            bar { id }
+          }
+          baz {
+            id
+            bar { id }
+          }
+        }`
+      ).snapshot;
+
+      extractResult = extract(snapshot);
+    });
+
+    it(`extract Json serialization object`, () => {
+      expect(extractResult).to.deep.eq({
+        [QueryRootId]: {
+          inbound: null,
+          outbound: [
+            { id: 'a', path: ['foo', 0] },
+            { id: 'a', path: ['foo', 1] },
+            { id: 'b', path: ['foo', 2] },
+            { id: 'a', path: ['foo', 3] },
+            { id: 'b', path: ['foo', 4] },
+            { id: 'a', path: ['baz'] },
+          ],
+          data: null,
+        },
+        '1': {
+          inbound: [
+            { id: 'a', path: ['bar'] },
+            { id: 'b', path: ['bar'] },
+          ],
+          outbound: null,
+          data: { id: 1 },
+        },
+        'a': {
+          inbound: [
+            { id: QueryRootId, path: ['foo', 0] },
+            { id: QueryRootId, path: ['foo', 1] },
+            { id: QueryRootId, path: ['foo', 3] },
+            { id: QueryRootId, path: ['baz'] },
+          ],
+          outbound: [{ id: 1, path: ['bar'] }],
+          data: {
+            id: 'a',
+          },
+        },
+        'b': {
+          inbound: [
+            { id: QueryRootId, path: ['foo', 2] },
+            { id: QueryRootId, path: ['foo', 4] },
+            { id: QueryRootId, path: ['baz'] },
+          ],
+          outbound: [{ id: 1, path: ['bar'] }],
+          data: {
+            id: 'b',
+          },
+        },
+      });
+    });
+
+  });
+});
