e5eb46f90624d0f608bdd0aa1ef7f133fc60deac
Yui T <yui@convoy.com>
2017-10-18 11:02:59 -0700

    extract: add false value

---
+++ test/unit/operations/extract/basic/falsyValues.ts
@@ -0,0 +1,32 @@
+import { extract } from '../../../../../src/operations/extract';
+import { Serializeable } from '../../../../../src/primitive';
+import { StaticNodeId } from '../../../../../src/schema';
+import { createSnapshot } from '../../../../helpers';
+
+
+const { QueryRoot: QueryRootId } = StaticNodeId;
+
+describe.skip(`operations.extract`, () => {
+  describe(`falsy values`, () => {
+
+    let extractResult: Serializeable.GraphSnapshot;
+    beforeAll(() => {
+      const snapshot = createSnapshot(
+        { null: null, false: false, zero: 0, string: '' },
+        `{ null, false, zero, string }`
+      ).snapshot;
+
+      extractResult = extract(snapshot);
+    });
+
+    it(`extract Json serializable object`, () => {
+      expect(extractResult).to.deep.eq({
+        [QueryRootId]: {
+          nodeSnapshotType: Serializeable.NodeSnapshotType.EntitySnapshot,
+          data: { null: null, false: false, zero: 0, string: '' },
+        },
+      });
+    });
+
+  });
+});
