90366e404364cc5e0ae840bd111a297534b08fe4
Ian MacLeod <ian@nevir.net>
2017-10-19 14:22:55 -0700

    Implement entityUpdaters

--- test/unit/context/CacheContext/entityUpdaters.ts
+++ test/unit/context/CacheContext/entityUpdaters.ts
@@ -1,6 +1,5 @@
+import { Cache } from '../../../../src';
 import { CacheContext } from '../../../../src/context';
-import { GraphSnapshot } from '../../../../src/GraphSnapshot';
-import { read, write } from '../../../../src/operations';
 import { query, strictConfig } from '../../../helpers';
 
 describe(`operations.read`, () => {
@@ -15,82 +14,79 @@ describe(`operations.read`, () => {
       }
     `);
 
-    const context = new CacheContext({
-      ...strictConfig,
-      entityUpdaters: {
-        User(dataProxy, user, previous) {
-          const nextActive = user && user.active;
-          const prevActive = previous && previous.active;
-          if (nextActive === prevActive) return;
+    const entityUpdaters: CacheContext.EntityUpdaters = {
+      User(dataProxy, user, previous) {
+        const nextActive = user && user.active;
+        const prevActive = previous && previous.active;
+        if (nextActive === prevActive) return;
 
-          const userId = user ? user.id : previous.id;
-          const { activeUsers } = dataProxy.readQuery({ query: activeUsersQuery.document });
-          let newActiveUsers: any[];
-          if (!nextActive) {
-            // Remove users once they're no longer active.
-            newActiveUsers = activeUsers.filter((activeUser: any) => activeUser.id !== userId);
-          } else if (activeUsers.findIndex((u: any) => u.id === userId) === -1) {
-            // Insert newly active users if they're not already in the list.
-            newActiveUsers = [...activeUsers, user];
-          } else {
-            return; // No change.
-          }
+        const userId = user ? user.id : previous.id;
+        const { activeUsers } = dataProxy.readQuery({ query: activeUsersQuery.document });
+        let newActiveUsers: any[];
+        if (!nextActive) {
+          // Remove users once they're no longer active.
+          newActiveUsers = activeUsers.filter((activeUser: any) => activeUser.id !== userId);
+        } else if (activeUsers.findIndex((u: any) => u.id === userId) === -1) {
+          // Insert newly active users if they're not already in the list.
+          newActiveUsers = [...activeUsers, user];
+        } else {
+          return; // No change.
+        }
 
-          if (newActiveUsers.length === activeUsers.length) return; // No change.
+        if (newActiveUsers.length === activeUsers.length) return; // No change.
 
-          dataProxy.writeQuery({
-            query: activeUsersQuery.document,
-            data: { activeUsers: newActiveUsers },
-          });
-        },
+        dataProxy.writeQuery({
+          query: activeUsersQuery.document,
+          data: { activeUsers: newActiveUsers },
+        });
       },
+    };
+
+    const userUpdater = jest.spyOn(entityUpdaters, 'User');
+
+    let cache: Cache;
+    beforeEach(() => {
+      cache = new Cache({ ...strictConfig, entityUpdaters });
+      cache.write(activeUsersQuery, {
+        activeUsers: [
+          { __typename: 'User', id: 1, name: 'Gouda', active: true },
+          { __typename: 'User', id: 2, name: 'Munster', active: true },
+        ],
+      });
+
+      userUpdater.mockClear();
     });
 
-    const baseSnapshot = write(context, new GraphSnapshot(), activeUsersQuery, {
-      activeUsers: [
-        { __typename: 'User', id: 1, name: 'Gouda', active: true },
-        { __typename: 'User', id: 2, name: 'Munster', active: true },
-      ],
-    }).snapshot;
-
     it(`triggers updaters when an entity is first seen`, () => {
-      const userUpdater = jest.spyOn(context.entityUpdaters, 'User');
-
-      write(context, baseSnapshot, { ...getUserQuery, variables: { id: 3 } }, {
+      cache.write({ ...getUserQuery, variables: { id: 3 } }, {
         user: { __typename: 'User', id: 3, name: 'Cheddar', active: true },
       });
 
-      const dataProxy = userUpdater.mock.calls[0][0];
-      expect(userUpdater.mock.calls).to.deep.eq([
-        dataProxy,
-        { __typename: 'User', id: 3, name: 'Cheddar', active: true },
-        undefined,
-      ]);
+      expect(userUpdater.mock.calls.length).to.eq(1);
+      const [, user, previous] = userUpdater.mock.calls[0];
+      expect(user).to.deep.eq({ __typename: 'User', id: 3, name: 'Cheddar', active: true });
+      expect(previous).to.deep.eq(undefined);
     });
 
     it(`triggers updaters when an entity is orphaned`, () => {
-      const userUpdater = jest.spyOn(context.entityUpdaters, 'User');
-
-      write(context, baseSnapshot, activeUsersQuery, {
+      cache.write(activeUsersQuery, {
         activeUsers: [
           { __typename: 'User', id: 2, name: 'Munster', active: true },
         ],
       });
 
-      const dataProxy = userUpdater.mock.calls[0][0];
-      expect(userUpdater.mock.calls).to.deep.eq([
-        dataProxy,
-        undefined,
-        { __typename: 'User', id: 1, name: 'Gouda', active: true },
-      ]);
+      expect(userUpdater.mock.calls.length).to.eq(1);
+      const [, user, previous] = userUpdater.mock.calls[0];
+      expect(user).to.eq(undefined);
+      expect(previous).to.deep.eq({ __typename: 'User', id: 1, name: 'Gouda', active: true });
     });
 
     it(`respects writes by updaters`, () => {
-      const { snapshot } = write(context, baseSnapshot, { ...getUserQuery, variables: { id: 2 } }, {
+      cache.write({ ...getUserQuery, variables: { id: 2 } }, {
         user: { __typename: 'User', id: 2, name: 'Munster', active: false },
       });
 
-      expect(read(context, activeUsersQuery, snapshot)).to.deep.eq({
+      expect(cache.read(activeUsersQuery).result).to.deep.eq({
         activeUsers: [
           { __typename: 'User', id: 1, name: 'Gouda', active: true },
         ],
