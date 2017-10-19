6ab4233f80872748d7f1364bec2c99c8d1734536
Yui T <yui@convoy.com>
2017-10-18 21:57:57 -0700

    Clean up invalidValues test

--- test/unit/operations/extract/basic/invalidValues.ts
+++ test/unit/operations/extract/basic/invalidValues.ts
@@ -1,16 +1,13 @@
+import { GraphSnapshot } from '../../../../../src/GraphSnapshot';
 import { extract } from '../../../../../src/operations/extract';
-import { Serializeable } from '../../../../../src/primitive';
-import { StaticNodeId } from '../../../../../src/schema';
 import { createSnapshot } from '../../../../helpers';
-
-const { QueryRoot: QueryRootId } = StaticNodeId;
 
 describe.skip(`operations.extract`, () => {
   describe(`invalid values`, () => {
 
     let snapshot: GraphSnapshot;
     beforeAll(() => {
-      const snapshot = createSnapshot(
+      snapshot = createSnapshot(
         { nan: NaN, func: (() => {}) as any },
         `{ nan, func}`
       ).snapshot;
