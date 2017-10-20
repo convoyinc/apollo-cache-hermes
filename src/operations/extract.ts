61b605c36f15140c7c1367d6d2f2c934999644a1
Yui T <yui@convoy.com>
2017-10-19 20:38:57 -0700

    minor fixes to make it work better with parameterized fields

--- src/operations/extract.ts
+++ src/operations/extract.ts
@@ -2,7 +2,7 @@ import * as _ from 'lodash'; // eslint-disable-line import/no-extraneous-depende
 
 import { GraphSnapshot } from '../GraphSnapshot';
 import { EntitySnapshot, NodeSnapshot, ParameterizedValueSnapshot } from '../nodes';
-import { JsonValue, JsonObject } from '../primitive';
+import { JsonValue } from '../primitive';
 import { Serializable } from '../schema';
 import { isScalar } from '../util';
 
@@ -53,7 +53,7 @@ export function extract(data: GraphSnapshot): Serializable.GraphSnapshot {
 }
 
 interface OutboundTree {
-  [key: string]: OutboundTree | OutboundTree[] | null;
+  [key: string]: OutboundTree | never[] | [OutboundTree] | null;
 }
 
 function createDataWithoutReferences(entity: NodeSnapshot): JsonValue | undefined {
@@ -80,12 +80,23 @@ function createDataWithoutReferences(entity: NodeSnapshot): JsonValue | undefine
     return entity.data;
   }
 
-  const outboundTree: OutboundTree = Object.create(null);
-
+  let outboundTree: OutboundTree | [OutboundTree] | undefined;
   let currentTreeNode: OutboundTree | [OutboundTree];
+
   for (const outboundRef of entity.outbound) {
     const outboundPath = outboundRef.path;
-    currentTreeNode = outboundTree;
+    if (outboundPath.length > 0) {
+      // entity.data can be array if
+      // the entity is ParameterizedValueNodeSnapshot
+      outboundTree = outboundTree
+        ? outboundTree
+        : entity.data instanceof Array ? [] : Object.create(null);
+      currentTreeNode = outboundTree!;
+    } else {
+      // We probably should check that when
+      // outboundPath.length === 0 -> there is only one outboundRef
+      break;
+    }
 
     for (let i = 0; i < outboundPath.length; ++i) {
       const currentPath = outboundPath[i];
@@ -112,28 +123,35 @@ function createDataWithoutReferences(entity: NodeSnapshot): JsonValue | undefine
     }
   }
 
-  return getValuesFromData(entity.data, outboundTree);
+  // There is no outboundTree so the entire data is just a reference
+  // see: topLevelParameterizedReference
+  return outboundTree ? getValuesFromDataProperty(entity.data, outboundTree) : undefined;
 }
 
 // TODO (yuisu): Handle top-level data is an array outside
-function getValuesFromData(data: JsonValue, outboundTree: OutboundTree): JsonValue {
+function getValuesFromDataProperty(data: JsonValue, outboundTree: OutboundTree | [OutboundTree]): JsonValue {
   if (isScalar(data)) {
     return data;
   }
 
   // ParameterizedValueNodeSnapshot can have array as top-level data value.
   if (data instanceof Array) {
-    // We can safely cast to JsonObject because if it is just a scalar value,
-    // we will just copy out already.
-    return data.map((element) => {
-      return getValuesFromData(element, outboundTree);
-    });
+    // For array outboundTree should only need to store
+    // one node per an entire array.
+    const result = new Array(data.length);
+    for (let i = 0; i < data.length; ++i) {
+      const element = data[i];
+      result[i] = element
+        ? getValuesFromDataProperty(element, outboundTree[0])
+        : null;
+    }
+    return result;
   }
 
-  // data is an object type
+  // Data is an object type
   const output = {};
   for (const dataName of Object.getOwnPropertyNames(data)) {
-    const outboundTreeNode = outboundTree[dataName];
+    const outboundTreeNode = (outboundTree as OutboundTree)[dataName];
     // Doesn't exist in outboundTree, it is a value
     if (outboundTreeNode === undefined) {
       output[dataName] = data[dataName];
@@ -141,11 +159,11 @@ function getValuesFromData(data: JsonValue, outboundTree: OutboundTree): JsonVal
       continue;
     } else if (_.isArray(outboundTreeNode)) {
       output[dataName] = outboundTreeNode.length === 0
-        ? [] : getValuesFromData(data[dataName], outboundTreeNode[0]);
+        ? [] : getValuesFromDataProperty(data[dataName], outboundTreeNode as [OutboundTree]);
     } else if (_.isPlainObject(outboundTreeNode)) {
       // exist in outboundTree as an object then
       // one of its children is a reference so walk it
-      output[dataName] = getValuesFromData(data[dataName], outboundTreeNode);
+      output[dataName] = getValuesFromDataProperty(data[dataName], outboundTreeNode);
     }
   }
   return output;
