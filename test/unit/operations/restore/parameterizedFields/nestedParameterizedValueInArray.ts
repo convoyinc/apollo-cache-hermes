bd4c246e78371e850a45607ded98694a1b92f85a
Yui T <yui@convoy.com>
2017-10-24 13:42:07 -0700

    restore: add parameterized tests

---
+++ test/unit/operations/restore/parameterizedFields/nestedParameterizedValueInArray.ts
@@ -0,0 +1,104 @@
+import { GraphSnapshot } from '../../../../../src/GraphSnapshot';
+import { EntitySnapshot, ParameterizedValueSnapshot } from '../../../../../src/nodes';
+import { restore } from '../../../../../src/operations';
+import { nodeIdForParameterizedValue } from '../../../../../src/operations/SnapshotEditor';
+import { StaticNodeId, Serializable } from '../../../../../src/schema';
+import { createGraphSnapshot, createStrictCacheContext } from '../../../../helpers';
+
+const { QueryRoot: QueryRootId } = StaticNodeId;
+
+describe.skip(`operations.restore`, () => {
+  describe(`nested parameterized value in an array`, () => {
+
+    const parameterizedId0 = nodeIdForParameterizedValue(
+      QueryRootId,
+      ['one', 'two', 0, 'three'],
+      { id: 1, withExtra: true }
+    );
+
+    const parameterizedId1 = nodeIdForParameterizedValue(
+      QueryRootId,
+      ['one', 'two', 1, 'three'],
+      { id: 1, withExtra: true }
+    );
+
+    let restoreGraphSnapshot: GraphSnapshot, originalGraphSnapshot: GraphSnapshot;
+    beforeAll(() => {
+      const cacheContext = createStrictCacheContext();
+      originalGraphSnapshot = createGraphSnapshot(
+        {
+          one: {
+            two: [
+              {
+                three: {
+                  name: 'Three0',
+                  extra: false,
+                },
+              },
+              {
+                three: {
+                  name: 'Three1',
+                  extra: true,
+                },
+              },
+            ],
+          },
+        },
+        `query getAFoo($id: ID!) {
+          one {
+            two {
+              three (id: $id, withExtra: true) {
+                name extra
+              }
+            }
+          }
+        }`,
+        cacheContext,
+        { id: 1 }
+      );
+
+      restoreGraphSnapshot = restore({
+        [QueryRootId]: {
+          type: Serializable.NodeSnapshotType.EntitySnapshot,
+          outbound: [
+            { id: parameterizedId0, path: ['one', 'two', 0, 'three'] },
+            { id: parameterizedId1, path: ['one', 'two', 1, 'three'] },
+          ],
+          data: {
+            one: {
+              // null is a place holder, original value is a spares array
+              two: [null, null],
+            },
+          },
+        },
+        [parameterizedId0]: {
+          type: Serializable.NodeSnapshotType.ParameterizedValueSnapshot,
+          inbound: [{ id: QueryRootId, path: ['one', 'two', 0, 'three'] }],
+          data: {
+            name: 'Three0',
+            extra: false,
+          },
+        },
+        [parameterizedId1]: {
+          type: Serializable.NodeSnapshotType.ParameterizedValueSnapshot,
+          inbound: [{ id: QueryRootId, path: ['one', 'two', 1, 'three'] }],
+          data: {
+            name: 'Three1',
+            extra: true,
+          },
+        },
+      }, cacheContext);
+    });
+
+    it(`restores GraphSnapshot from JSON serializable object`, () => {
+      expect(restoreGraphSnapshot).to.deep.eq(originalGraphSnapshot);
+    });
+
+    it(`correctly restores different types of NodeSnapshot`, () => {
+      expect(restoreGraphSnapshot.getNodeSnapshot(QueryRootId)).to.be.an.instanceOf(EntitySnapshot);
+      expect(restoreGraphSnapshot.getNodeSnapshot(parameterizedId0)).to.be.an.instanceof(ParameterizedValueSnapshot);
+      expect(restoreGraphSnapshot.getNodeSnapshot(parameterizedId1)).to.be.an.instanceof(ParameterizedValueSnapshot);
+    });
+
+  });
+});
---
+++ test/unit/operations/restore/parameterizedFields/nestedParameterizedValueWithArrayOfNestedValues.ts
@@ -0,0 +1,88 @@
+import { GraphSnapshot } from '../../../../../src/GraphSnapshot';
+import { EntitySnapshot, ParameterizedValueSnapshot } from '../../../../../src/nodes';
+import { restore } from '../../../../../src/operations';
+import { nodeIdForParameterizedValue } from '../../../../../src/operations/SnapshotEditor';
+import { StaticNodeId, Serializable } from '../../../../../src/schema';
+import { createGraphSnapshot, createStrictCacheContext } from '../../../../helpers';
+
+const { QueryRoot: QueryRootId } = StaticNodeId;
+
+describe.skip(`operations.restore`, () => {
+  describe(`nested parameterized value with an array of nested values`, () => {
+
+    const parameterizedId = nodeIdForParameterizedValue(
+      QueryRootId,
+      ['one', 'two'],
+      { id: 1 }
+    );
+
+    let restoreGraphSnapshot: GraphSnapshot, originalGraphSnapshot: GraphSnapshot;
+    beforeAll(() => {
+      const cacheContext = createStrictCacheContext();
+      originalGraphSnapshot = createGraphSnapshot(
+        {
+          one: {
+            two: [
+              {
+                three: {
+                  threeValue: 'first',
+                },
+              },
+              {
+                three: {
+                  threeValue: 'second',
+                },
+              },
+              null,
+            ],
+          },
+        },
+        `query nested($id: ID!) {
+          one {
+            two(id: $id) {
+              three {
+                threeValue
+              }
+            }
+          }
+        }`,
+        cacheContext,
+        { id: 1 }
+      );
+
+      restoreGraphSnapshot = restore({
+        [QueryRootId]: {
+          type: Serializable.NodeSnapshotType.EntitySnapshot,
+          outbound: [{ id: parameterizedId, path: ['one', 'two'] }],
+        },
+        [parameterizedId]: {
+          type: Serializable.NodeSnapshotType.ParameterizedValueSnapshot,
+          inbound: [{ id: QueryRootId, path: ['one', 'two'] }],
+          data: [
+            {
+              three: {
+                threeValue: 'first',
+              },
+            },
+            {
+              three: {
+                threeValue: 'second',
+              },
+            },
+            null,
+          ],
+        },
+      }, cacheContext);
+    });
+
+    it(`restores GraphSnapshot from JSON serializable object`, () => {
+      expect(restoreGraphSnapshot).to.deep.eq(originalGraphSnapshot);
+    });
+
+    it(`correctly restores different types of NodeSnapshot`, () => {
+      expect(restoreGraphSnapshot.getNodeSnapshot(QueryRootId)).to.be.an.instanceOf(EntitySnapshot);
+      expect(restoreGraphSnapshot.getNodeSnapshot(parameterizedId)).to.be.an.instanceof(ParameterizedValueSnapshot);
+    });
+
+  });
+});
