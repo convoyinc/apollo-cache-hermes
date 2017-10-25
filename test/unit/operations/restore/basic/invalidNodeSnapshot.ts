ac98dd27b766e322e16bb452e272dc377b0d8077
Yui T <yui@convoy.com>
2017-10-24 17:19:27 -0700

    Restore: add invalid NodeSnapshot type

---
+++ test/unit/operations/restore/basic/invalidNodeSnapshot.ts
@@ -0,0 +1,30 @@
+import { restore } from '../../../../../src/operations';
+import { Serializable, StaticNodeId } from '../../../../../src/schema';
+import { createStrictCacheContext } from '../../../../helpers';
+
+const { QueryRoot: QueryRootId } = StaticNodeId;
+
+describe.skip(`operations.restore`, () => {
+  describe(`invalid NodeSnapshot type`, () => {
+
+    it(`throws error when restore invalid NodeSnapshot type`, () => {
+      expect(() => {
+        const cacheContext = createStrictCacheContext();
+        restore({
+          [QueryRootId]: {
+            type: Serializable.NodeSnapshotType.EntitySnapshot,
+            outbound: [{ id: '1', path: ['foo'] }],
+            data: { },
+          },
+          '1': {
+            type: -1,
+            data: {
+              INVALID: 42,
+            },
+          },
+        }, cacheContext);
+      }).to.throw(/Invalid Serializable.NodeSnapshotType/i);
+    });
+
+  });
+});
