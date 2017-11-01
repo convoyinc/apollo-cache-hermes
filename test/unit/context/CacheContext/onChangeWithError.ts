78bcaf87ed7ee6257090ecbe19a2e621b42a9eed
Yui T <yui@convoy.com>
2017-11-01 14:56:23 -0700

    Add onChange tests

---
+++ test/unit/context/CacheContext/onChangeWithError.ts
@@ -0,0 +1,48 @@
+import { Cache } from '../../../../src';
+import { query } from '../../../helpers';
+
+describe(`context.CacheContext`, () => {
+  describe(`onChange callback on error`, () => {
+    const mockOnChange = jest.fn();
+    const graphqlQuery = query(`{
+      foo {
+        id
+        bar {
+          id
+          name
+        }
+      }
+    }`);
+
+    const cache = new Cache({
+      logger: {
+        debug: jest.fn(),
+        warn: jest.fn(),
+        error: jest.fn(),
+        group: jest.fn(),
+        groupEnd: jest.fn(),
+      },
+      onChange: mockOnChange,
+    });
+
+    it(`do not trigger onChange callback on error`, () => {
+      cache.transaction((transaction) => {
+        transaction.write(
+          graphqlQuery,
+          {
+            foo: {
+              id: 0,
+              bar: {
+                id: 1,
+                name: 'Gouda',
+              },
+            },
+          }
+        );
+        throw new Error(`Fake error`);
+      });
+      expect(mockOnChange.mock.calls.length).to.equal(0);
+    });
+
+  });
+});
