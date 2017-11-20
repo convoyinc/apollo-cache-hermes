6fe8114d0e15c68f2fbf0e12aed618305497e634
Yui T <yui@convoy.com>
2017-11-20 13:59:20 -0800

    Add readFragment tests

---
+++ test/unit/Hermes/readFragment/parameterizedValue.ts
@@ -0,0 +1,87 @@
+import gql from 'graphql-tag';
+
+import { Hermes } from '../../../../src/apollo/Hermes';
+import { CacheContext } from '../../../../src/context/CacheContext';
+import { nodeIdForParameterizedValue } from '../../../../src/operations/SnapshotEditor';
+import { StaticNodeId, Serializable } from '../../../../src/schema';
+import { strictConfig } from '../../../helpers/context';
+
+const { QueryRoot: QueryRootId } = StaticNodeId;
+
+describe(`Hermes`, () => {
+  describe(`readFragment`, () => {
+
+    let hermes: Hermes;
+    beforeAll(() => {
+      hermes = new Hermes(new CacheContext(strictConfig));
+      const parameterizedId = nodeIdForParameterizedValue(
+        '123',
+        ['shipment'],
+        { city: 'Seattle' }
+      );
+
+      hermes.restore({
+        [QueryRootId]: {
+          type: Serializable.NodeSnapshotType.EntitySnapshot,
+          outbound: [{ id: '123', path: ['viewer'] }],
+          data: {
+            justValue: '42',
+          },
+        },
+        '123': {
+          type: Serializable.NodeSnapshotType.EntitySnapshot,
+          inbound: [{ id: QueryRootId, path: ['viewer'] }],
+          outbound: [{ id: parameterizedId, path: ['shipment'] }],
+          data: {
+            id: 123,
+            name: 'Gouda',
+            __typename: 'Viewer',
+          },
+        },
+        [parameterizedId]: {
+          type: Serializable.NodeSnapshotType.ParameterizedValueSnapshot,
+          inbound: [{ id: '123', path: ['shipment'] }],
+          data: {
+            __typename: 'Shipment',
+            destination: 'Seattle',
+            complete: false,
+            truckType: 'flat-bed',
+          },
+        },
+      });
+    });
+
+    it(`correctly read a fragment with parameterized value`, () => {
+      expect(hermes.readFragment({
+        id: '123',
+        fragment: gql(`
+          fragment viewer on Viewer {
+            id
+            name
+            __typename
+            shipment(city: $city) {
+              __typename
+              truckType
+              complete
+              destination
+            }
+          }
+        `),
+        variables: {
+          city: 'Seattle',
+        },
+      })).to.be.deep.eq({
+        id: 123,
+        name: 'Gouda',
+        __typename: 'Viewer',
+        shipment: {
+          __typename: 'Shipment',
+          destination: 'Seattle',
+          complete: false,
+          truckType: 'flat-bed',
+        },
+      });
+    });
+
+  });
+});
