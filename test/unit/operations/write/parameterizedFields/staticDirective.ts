d1bbb0c835765dd7a758150c7d16b2b3a3bb8bb5
Ian MacLeod <ian@nevir.net>
2017-12-26 14:03:18 -0800

    Tests for @static

---
+++ test/unit/operations/write/parameterizedFields/staticDirective.ts
@@ -0,0 +1,76 @@
+import { CacheContext } from '../../../../../src/context';
+import { GraphSnapshot } from '../../../../../src/GraphSnapshot';
+import { write } from '../../../../../src/operations/write';
+import { StaticNodeId } from '../../../../../src/schema';
+import { query, strictConfig } from '../../../../helpers';
+
+const { QueryRoot: QueryRootId } = StaticNodeId;
+
+describe(`operations.write`, () => {
+
+  const context = new CacheContext(strictConfig);
+  const empty = new GraphSnapshot();
+
+  describe(`with @static`, () => {
+
+    describe(`with @static fields`, () => {
+
+      const staticQuery = query(`{
+        todos {
+          id
+          value
+          history(limit: 2) @static {
+            changeType
+            value
+          }
+        }
+      }`);
+
+      let snapshot: GraphSnapshot;
+      beforeAll(() => {
+        snapshot = write(context, empty, staticQuery, {
+          todos: [
+            {
+              id: 1,
+              value: 'hello',
+              history: [
+                {
+                  changeType: 'edit',
+                  value: 'ohai',
+                },
+                {
+                  changeType: 'edit',
+                  value: 'hey',
+                },
+              ],
+            },
+          ],
+        }).snapshot;
+      });
+
+      it(`writes static fields to the containing entity`, () => {
+        expect(snapshot.getNodeData('1')).to.deep.eq({
+          id: 1,
+          value: 'hello',
+          history: [
+            {
+              changeType: 'edit',
+              value: 'ohai',
+            },
+            {
+              changeType: 'edit',
+              value: 'hey',
+            },
+          ],
+        });
+      });
+
+      it(`does not create parameterized field nodes`, () => {
+        expect(snapshot.allNodeIds()).to.have.members([QueryRootId, '1']);
+      });
+
+    });
+
+  });
+
+});
