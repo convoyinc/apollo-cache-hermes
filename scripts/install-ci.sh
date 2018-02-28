7307b14a83fca9af67d3efcee4253397505242ef
Ian MacLeod <ian@nevir.net>
2018-02-28 15:04:57 -0800

    Let yarn do what it's good at

--- scripts/install-ci.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# If there are already node_modules, we can stop here.  We expect that our CI
-# build process restores them only when package.json has not changed.
-[[ -d ./node_modules ]] && exit 0
-
-# Finally, install.
-yarn install
