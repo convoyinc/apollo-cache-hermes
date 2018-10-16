53771216311ce0d3772385ef06f1acd7a8875ccd
monotkate <miranda@convoy.com>
2018-10-15 17:36:14 -0700

    add note

--- test/env/base.ts
+++ test/env/base.ts
@@ -4,9 +4,14 @@ import chaiJestDiff from 'chai-jest-diff';
 
 // Chai
 
-// We prefer Chai's `expect` interface.
+// We are transitioning to using Jasmine for our test expects.  During the
+// transition, Jasmine and chai will be run in parallel using jestExpect for
+// Jasmine, and the default expect for chai.  Once complete, Jasmine will
+// replace the chai global.expect.
 global.jestExpect = global.expect;
+// We prefer Chai's `expect` interface.
 global.expect = chai.expect;
+
 // Give us all the info!
 chai.config.truncateThreshold = 0;
 
