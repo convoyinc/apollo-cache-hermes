b70dcedaaa63a88a274b376b817b4ba0df75f2f2
monotkate <miranda@convoy.com>
2018-10-20 16:59:22 -0700

    remove comment

--- test/env/base.ts
+++
@@ -1,5 +0,0 @@
-// We are transitioning to using Jasmine for our test expects.  During the
-// transition, Jasmine and chai will be run in parallel using expect for
-// Jasmine, and the default expect for chai.  Once complete, Jasmine will
-// replace the chai global.expect.
-// global.expect = global.expect;
