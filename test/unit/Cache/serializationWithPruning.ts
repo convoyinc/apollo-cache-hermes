be39013154dd5fa0a8b2a67801f656fd4b9c912d
monotkate <miranda@convoy.com>
2018-10-20 16:18:44 -0700

    convert Cache tests to Jasmine

--- test/unit/Cache/serializationWithPruning.ts
+++ test/unit/Cache/serializationWithPruning.ts
@@ -83,7 +83,7 @@ describe(`serialization with pruning`, () => {
     newCache.restore(JSON.parse(storedExtractResult));
 
     // the restored cache should look as if muddyQuery never happens
-    expect(newCache.getSnapshot()).to.deep.eq(originalCacheSnapshot);
+    jestExpect(newCache.getSnapshot()).toEqual(originalCacheSnapshot);
   });
 
   it(`can extract the cache with slightly trimmed 'three' object`, () => {
@@ -133,7 +133,7 @@ describe(`serialization with pruning`, () => {
 
     // the restored cache should look as if it is built up from scrach with
     // altPruneQuery
-    expect(newCache.getSnapshot()).to.deep.eq(expectedCache.getSnapshot());
+    jestExpect(newCache.getSnapshot()).toEqual(expectedCache.getSnapshot());
   });
 
 });
--- test/unit/Cache/watchStaticFields.ts
+++ test/unit/Cache/watchStaticFields.ts
@@ -69,10 +69,10 @@ describe(`Cache#watch`, () => {
     const updates: QueryResult[] = [];
     cache.watch(simpleGraph, newResult => updates.push(newResult));
 
-    expect(updates.length).to.eq(1);
+    jestExpect(updates.length).toBe(1);
     const [update] = updates;
-    expect(update.result).to.deep.eq(baseState);
-    expect(update.complete).to.eq(true);
+    jestExpect(update.result).toEqual(baseState);
+    jestExpect(update.complete).toBe(true);
   });
 
   it(`triggers a callback after writing the same query with new values`, () => {
@@ -80,11 +80,11 @@ describe(`Cache#watch`, () => {
     cache.watch(simpleGraph, newResult => updates.push(newResult));
     cache.write(simpleGraph, { foo: { id: 1, bar: { id: 3, name: 'bar' } } });
 
-    expect(updates.length).to.eq(2);
+    jestExpect(updates.length).toBe(2);
 
     const [, update] = updates;
-    expect((update.result as any).foo.bar.id).to.eq(3);
-    expect(update.complete).to.eq(true);
+    jestExpect((update.result as any).foo.bar.id).toBe(3);
+    jestExpect(update.complete).toBe(true);
   });
 
   it(`doesn't trigger a callback if unrelated entities change`, () => {
@@ -92,7 +92,7 @@ describe(`Cache#watch`, () => {
     cache.watch(simpleGraph, newResult => updates.push(newResult));
     cache.write(partialOverlap, { foo: { id: 1, baz: { id: 3, name: 'baz2' } } });
 
-    expect(updates.length).to.eq(1);
+    jestExpect(updates.length).toBe(1);
   });
 
   it(`triggers an update on indirect edits to an entity`, () => {
@@ -100,10 +100,10 @@ describe(`Cache#watch`, () => {
     cache.watch(simpleGraph, newResult => updates.push(newResult));
     cache.write(indirectEdit, { thing: { id: 2, name: 'bar2' } });
 
-    expect(updates.length).to.eq(2);
+    jestExpect(updates.length).toBe(2);
     const [, update] = updates;
-    expect((update.result as any).foo.bar.name).to.eq('bar2');
-    expect(update.complete).to.eq(true);
+    jestExpect((update.result as any).foo.bar.name).toBe('bar2');
+    jestExpect(update.complete).toBe(true);
   });
 
   it(`triggers an update on reference updates from the query root`, () => {
@@ -111,10 +111,10 @@ describe(`Cache#watch`, () => {
     cache.watch(simpleGraph, newResult => updates.push(newResult));
     cache.write(simpleGraph, { foo: { id: 100, bar: { id: 2, name: 'bar' } } });
 
-    expect(updates.length).to.eq(2);
+    jestExpect(updates.length).toBe(2);
     const [, update] = updates;
-    expect((update.result as any).foo.id).to.eq(100);
-    expect(update.complete).to.eq(true);
+    jestExpect((update.result as any).foo.id).toBe(100);
+    jestExpect(update.complete).toBe(true);
   });
 
   it(`handles transitions from complete to incomplete`, () => {
@@ -122,11 +122,11 @@ describe(`Cache#watch`, () => {
     cache.watch(simpleGraph, newResult => updates.push(newResult));
     cache.write(partialOverlap, { foo: { id: 100, baz: { id: 3, name: 'baz' } } });
 
-    expect(updates.length).to.eq(2);
+    jestExpect(updates.length).toBe(2);
     const [, update] = updates;
-    expect((update.result as any).foo.id).to.eq(100);
-    expect((update.result as any).foo.bar).to.eq(undefined);
-    expect(update.complete).to.eq(false);
+    jestExpect((update.result as any).foo.id).toBe(100);
+    jestExpect((update.result as any).foo.bar).toBe(undefined);
+    jestExpect(update.complete).toBe(false);
   });
 
   it(`handles transitions from incomplete to complete`, () => {
@@ -136,11 +136,11 @@ describe(`Cache#watch`, () => {
     cache2.watch(simpleGraph, newResult => updates.push(newResult));
     cache2.write(simpleGraph, { foo: { id: 1, bar: { id: 2, name: 'bar' } } });
 
-    expect(updates.length).to.eq(2);
+    jestExpect(updates.length).toBe(2);
     const [initial, update] = updates;
 
-    expect(initial.complete).to.eq(false);
-    expect(update.complete).to.eq(true);
+    jestExpect(initial.complete).toBe(false);
+    jestExpect(update.complete).toBe(true);
   });
 
 });
